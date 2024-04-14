e20b10a1acf34de40b42471fac47f7fc8e224e88
huxf <hxf168482@gmail.com>
2024-04-14 23:06:26 +0800

    chore: update files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import image from "@astrojs/image";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [sitemap(), image()]
+  integrations: [sitemap(), image(), tailwind()]
 });
